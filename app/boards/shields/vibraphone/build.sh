69b6f7ebe881ce380118a8bfb60c5afa6b43f09a
Gerry Hernandez <gerry@gerryhernandez.com>
2023-02-13 16:53:11 -0500

    Tweak brackets and autoshift. Now all symbols will autoshift.

--- app/boards/shields/vibraphone/build.sh
+++ app/boards/shields/vibraphone/build.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
+set -e
 
-# cd to ./app
-cd ../../../
+# pushd to ./app
+pushd ../../../
 
 if [ "$ZEPHYR_BASE" == "" ]; then
-  pushd ../zephyr
-  source zephyr-env.sh
-  popd
+  source ../zephyr/zephyr-env.sh
 fi
 
 west build -d build/left -b nice_nano_v2 -- -DSHIELD=vibraphone_left
+
+popd
+
+MOUNT_POINT="/Volumes/NICENANO"
+until [ -d "$MOUNT_POINT" ]
+do
+  sleep 1
+  echo "Waiting for $MOUNT_POINT to be mounted..."
+done
+
+cp ../../../../app/build/left/zephyr/zmk.uf2 "$MOUNT_POINT"
+
+echo "Done flashing!"
